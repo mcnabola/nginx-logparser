bc94f426408ef6cf0d16e77f0c7ac825e792e6a1
mcnabola <17226163@studentmail.ul.ie>
2021-04-01 18:50:21 +0100

    current location checks of persistance added

--- tailsimple.py
+++ tailsimple.py
@@ -4,11 +4,16 @@ from datetime import datetime
 
 lineformat = re.compile(r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (?P<refferer>-|"([^"]+)") (["](?P<useragent>[^"]+)["])""", re.IGNORECASE)
 
+date_format = "%d/%b/%Y:%H:%M:%S %z" 
+
+
+raw_date = "01/Apr/2021:04:17:59 +0000"
+date = datetime.strptime(raw_date, date_format)
+
 def parse(line):
     data = re.search(lineformat, line)
     if data:
         datadict = data.groupdict()
-        print("before NEW")
         return datadict
     else:
         return None
@@ -18,6 +23,7 @@ def persist():
     print("persist")
 
 def watch(fn, words):
+    global date
     fp = open(fn, 'r')
     while True:
         new = fp.readline()
@@ -28,8 +34,10 @@ def watch(fn, words):
             print(new)
             datadict = parse(new)
             if datadict is not None:
-                for key, value in datadict.items():
-                    print(key,value)
+                tempDate = datetime.strptime(datadict["dateandtime"], date_format)
+                if tempDate > date:
+                    print("In d hoi")
+                    date = tempDate#this should probably be done later like after the request has sent so we know that we can actually mark this off as being complete
 
                 #convert datestring to datetime obj 
             for word in words:###this can be the check here to see if the line is new
@@ -41,6 +49,12 @@ def watch(fn, words):
 
 fn = 'accesslog.txt'
 words = ['word']
+
+#hardcoded datetime to start the persisting from
+#this raw data could be recovered from a textfile or passed in as param
+# - now at the top 
+
+
 for hit_word, hit_sentence in watch(fn, words):
     print ("Found %r in line: %r" % (hit_word, hit_sentence))
 
